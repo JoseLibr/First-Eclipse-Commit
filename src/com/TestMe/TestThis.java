f1af14a9cdd003ffc02a532b5fdfa5d61c5d4cb7
JoseLibr <librado_5@yahoo.com>
2023-12-08 06:51:24 -0800

    first eclipse commit

---
+++ src/com/TestMe/TestThis.java
@@ -0,0 +1,20 @@
+package com.TestMe;
+
+public class TestThis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!"); 
+
+		System.out.println("Hello World!");
+
+	
+	
+	
+	
+	}
+	}
+
+		 
+	
